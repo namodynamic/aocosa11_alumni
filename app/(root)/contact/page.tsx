c355f578c83d5b91597d998f4104106fdc04d53b
namodynamic <nnamdi4u09@gmail.com>
2023-11-27 19:24:07 +0100

    first commit

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailjsServiceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+
+  const emailjsTemplateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+
+  const emailjsPublicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .send(
+        emailjsServiceId,
+        emailjsTemplateId,
+        {
+          from_name: form.name,
+          to_name: "Aocosa11",
+          from_email: form.email,
+          to_email: "aocosa2011@gmail.com",
+          message: form.message,
+        },
+        emailjsPublicKey
+      )
+      .then(() => {
+        setIsLoading(false);
+        alert("Thank you. We will get back to you as soon as possible");
+
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col padding-x xl:flex-row py-40 max-sm:mt-12 gap-4">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="sm:text-5xl text-3xl font-semibold sm:leading-snug">
+          Get in Touch with us
+        </h1>
+
+        <form
+          ref={formRef}
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+              className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card"
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+              className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card"
+              value={form.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows={4}
+              placeholder="Let us know how we can help you!"
+              required
+              className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card"
+              value={form.message}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="text-white bg-gradient-to-tr from-[#00c6ff] to-[#0072ff] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center shadow-card"
+            disabled={isLoading}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full mt-7 lg:h-auto md:h-[550px] h-[350px]">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15846.34518432835!2d3.8964472231710774!3d6.820064813186302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10396feaaa8daa97%3A0xcdd97b11613c7dd5!2sAdeola%20Odutola%20College%20(Public)!5e0!3m2!1sen!2sng!4v1701099157933!5m2!1sen!2sng"
+          width="600"
+          height="450"
+          className="w-full h-full border-0"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
